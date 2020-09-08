d37fa04bbd7ecba205a8823c7fea23019974554a
Haseeba-waheed <70893080+Haseeba-waheed@users.noreply.github.com>
2020-09-08 13:56:27 +0500

    Screenshort code modification

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,5 +1,5 @@
 package base;
-import jdk.incubator.jpackage.internal.IOUtils;
+import org.apache.commons.io.FileUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,53 +17,79 @@ import java.io.IOException;
 public class BaseTest {
     static ExtentTest test;
     static ExtentReports report;
-    private static IOUtils FileUtils;
+   static WebDriver driver;
+   // private static IOUtils FileUtils;
 
     @BeforeClass
     public static void startTest()
     {
-        report = new ExtentReports(System.getProperty("user.dir")+"\\ExtentReportResults.html");
-        test = report.startTest("ExtentDemo");
-    }
-    @Test
-    public void extentReportsDemo()
-    {
         System.setProperty("webdriver.chrome.driver", ".\\resources\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.co.in");
-        if(driver.getTitle().equals("Goooogle"))
+        driver = new ChromeDriver();
+
+        report = new ExtentReports(System.getProperty("user.dir")+"\\ExtentReportResults.html");
+        test = report.startTest("startTest","Before test");
+        test.log(LogStatus.PASS, "Setup completed");
+    }
+
+    @Test
+    public void extentReportsDemo()throws IOException
+    {
+        System.out.println("Test Case One with Thread Id:- "
+                + Thread.currentThread().getId());
+        test = report.startTest("extentReportsDemo","extentReportsDemo started");
+
+        driver.get("https://www.yahoo.com");
+
+        if(driver.getTitle().equals("Google"))
         {
+            System.out.println("extentReportsDemo title matched");
+
             test.log(LogStatus.PASS, "Navigated to the specified URL");
         }
         else
         {
-            test.log(LogStatus.FAIL, "Test Failed");
+            System.out.println("extentReportsDemo title not matched");
+
+            //test.log(LogStatus.FAIL, "Test Failed");
+            //System.out.println("Demo :: "+capture(driver));
+            test.log(LogStatus.FAIL, test.addScreenCapture(capture(driver))+"Test Failed");
         }
     }
+
+
     @Test
     public void extentReportsDemo2() throws IOException {
-        System.setProperty("webdriver.chrome.driver", ".\\resources\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+
+        System.out.println("Test Case  two with Thread Id:- "
+                + Thread.currentThread().getId());
+        test = report.startTest("extentReportsDemo2","extentReportsDemo2 started");
+
         driver.get("https://www.google.co.in");
         if(driver.getTitle().equals("Google"))
         {
+            System.out.println("extentReportsDemo2 title matched");
             test.log(LogStatus.PASS, "Navigated to the specified URL");
         }
         else {
-            test.log(LogStatus.FAIL, "Test Failed");
-            //test.log(LogStatus.FAIL, test.addScreenCapture("C:\\Users\\Haseeba.waheed\\Documents\\IntelliJ\\src\\images")+"Test Failed");
+            System.out.println("extentReportsDemo2 title not matched");
+
+           // test.log(LogStatus.FAIL, "Test Failed");
+            //capture(driver);
+
+          test.log(LogStatus.FAIL, test.addScreenCapture( capture(driver))+"Test Failed");
 
         }
 
     }
-  /*  public static String capture(WebDriver driver) throws IOException {
+      public static String capture(WebDriver driver) throws IOException {
         File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        File Dest = new File("src/../ErrImages/" + System.currentTimeMillis()
+        File Dest = new File(".\\src\\images\\" + System.currentTimeMillis()
                 + ".png");
         String errflpath = Dest.getAbsolutePath();
         FileUtils.copyFile(scrFile, Dest);
+        System.out.println("capture" +errflpath);
         return errflpath;
-    }*/
+    }
 
 
     @AfterClass
